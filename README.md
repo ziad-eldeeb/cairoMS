785c41ded4670f2525d9accb5d4288f4f89a898f
ziad-eldeeb <121318302+ziad-eldeeb@users.noreply.github.com>
2023-06-10 02:00:24 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cairoMS
+﻿# SE_starter_code
+ Run npm install to install all dependences 
+ npm start to run project
---
+++ connectors/seed.sql
@@ -0,0 +1,11 @@
+-- Insert Roles
+INSERT INTO se_project.roles("role")
+	VALUES ('user');
+INSERT INTO se_project.roles("role")
+	VALUES ('admin');
+INSERT INTO se_project.roles("role")
+	VALUES ('senior');	
+-- Set user role as Admin
+UPDATE se_project.users
+	SET "roleid"=2
+	WHERE "email"='desoukya@gmail.com';
